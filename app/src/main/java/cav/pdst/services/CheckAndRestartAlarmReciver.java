0e53b7070e40cff3efaec7db0d16278754a4b253
CavInc <cavinc20@gmail.com>
2017-08-21 22:46:10 +0700

    Работа над уведомлениями об окончании

--- app/src/main/java/cav/pdst/services/CheckAndRestartAlarmReciver.java
+++ app/src/main/java/cav/pdst/services/CheckAndRestartAlarmReciver.java
@@ -8,8 +8,13 @@ import android.content.Intent;
 
 import java.util.Calendar;
 
+import cav.pdst.data.managers.DataManager;
+
 public class CheckAndRestartAlarmReciver extends BroadcastReceiver {
+    private DataManager mDataManager;
+
     public CheckAndRestartAlarmReciver() {
+        mDataManager = DataManager.getInstance();
     }
 
     @Override
@@ -22,9 +27,16 @@ public class CheckAndRestartAlarmReciver extends BroadcastReceiver {
     // перезапускает будильник на следующий день
     private void restartAlarm(Context context){
         Calendar c = Calendar.getInstance();
+        c.add(Calendar.DATE,1);
+        c.set(Calendar.HOUR_OF_DAY,23);
+        c.set(Calendar.MINUTE,59);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context,CheckAndRestartAlarmReciver.class);
         PendingIntent pi= PendingIntent.getBroadcast(context,0, intent,0);
         am.set(AlarmManager.RTC_WAKEUP,c.getTimeInMillis(),pi);
+    }
+
+    private void showNotification(Context context){
+
     }
 }
